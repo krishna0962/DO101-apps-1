c488e1848a8b85c8313006af5f834ed6551fe054
krishna0962 <krishlivadoliya1399@gmail.com>
2020-06-05 16:37:46 +0530

    openshift weather app

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-const express = require('express');
-app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var response;
+var indexRouter = require('./routes/index');
 
-app.get('/', function (req, res) {
+var app = express();
 
-    response = 'This is version 3 of the app.' + '\n';
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-    //send the response to the client
-    res.send(response);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(8080, function () {
-  console.log('Server listening on port 8080...');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
